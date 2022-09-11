b65fb186e5a38aae4a2c28ddf66fadc3b9e032bc
toastedwedge <88474004+toastedwedge@users.noreply.github.com>
2022-09-10 22:49:35 -0500

    It's all broken, please don't run this.

--- CSharpShell/Program.cs
+++ CSharpShell/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace CSharpShell
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace CSharpShell
 {
     public class CSharpShell
     {
+        private static int exitCount = 0;
+        private static void Help()
+        {
+            Console.WriteLine("This is a help message");
+        }
+
+        private static void DoStuff(string args)
+        {
+            var arr = args.Split('\u002C');
+            foreach (var item in arr)
+            {
+                // do stuff
+                Process p = new Process();
+                p.StartInfo = new ProcessStartInfo(item);
+                p.Start();
+            }
+        }
+
+        private static void Loop()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var args = Console.ReadLine();
+                switch (args)
+                {
+                    case "help":
+                        Help();
+                        break;
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        DoStuff(args);
+                        break;
+                }
+            }
+        }
+
         internal static void Main()
         {
-            Console.WriteLine("Slap the Earth!");
+            const string HelloMsg = "Slap the Earth!";
+            Console.WriteLine(HelloMsg);
+            Loop();
         }
     }
 }
